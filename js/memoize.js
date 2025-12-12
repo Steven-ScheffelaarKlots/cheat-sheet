d4a5cd1f917142025512a762309a4a3eeebd69ab
Steven Scheffelaar Klots <steven@scheffelaar.com>
2025-12-12 10:47:33 -0500

    Update memoize with usage

--- js/memoize.js
+++ js/memoize.js
@@ -3,6 +3,7 @@ function memoize(fn) {
     return function(...args) {
         const key = JSON.stringify(args);
         if (cache.has(key)) {
+            console.log('Using a memoized value')
             return cache.get(key);
         }
         const result = fn(...args);
@@ -10,5 +11,18 @@ function memoize(fn) {
         return result;
     };
 }
+
+const expensiveComputation = (num) => {
+    // Simulate an expensive computation
+    let result = 0;
+    result = num * 50;
+    return result
+}
+
+const memoizedFunction = memoize(expensiveComputation)
+
+memoizedFunction(1)
+memoizedFunction(2)
+memoizedFunction(1)
 
 module.exports = memoize;
